f08bc3c5c4f1f261f4547896e2346769cdedf2ea
Anton Senko-Gaykevich <a.senko-gaykevich@itransition.com>
2021-03-07 23:45:26 +0200

    Added @RequestMapping("/quantum") to CheckController

--- src/main/kotlin/by/senko/gaykevich/quantumservice/controller/CheckController.kt
+++ src/main/kotlin/by/senko/gaykevich/quantumservice/controller/CheckController.kt
@@ -1,6 +1,7 @@
 package by.senko.gaykevich.quantumservice.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController
  * @since 2021-02-26
  */
 @RestController
+@RequestMapping("/quantum")
 class CheckController {
 
     @GetMapping("/check")
